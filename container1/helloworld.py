56b6dca80e2fdc68b98bf967842f87d75ee244bd
kabbilkarthi <g.i.kabbil@gmail.com>
2023-08-26 20:34:18 +0530

    1ST Application

---
+++ container1/helloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print(
+    """\
+Content-Type: text/html
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<h1>Hello, World!</h1>
+<p>This is to let you know that Kabbil took his 1st step in learning Docker.</p>
+</body>
+</html>"""
+)
